7fe597e24883d4e454a13bdabc4c6840aad8a387
AlbertoParente <albertoparente20@gmail.com>
2020-04-13 03:30:14 -0300

    Adding method from input text

--- src/main/java/br/com/compiler/scanner/CalculatorTest.java
+++ src/main/java/br/com/compiler/scanner/CalculatorTest.java
@@ -2,11 +2,17 @@ package br.com.compiler.scanner;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) throws IOException {
-        String expression = "-(8+12)**1*3/-4";
-        Lexer lexer = new Lexer(new StringReader(expression));
+        // String expression = "-(8+12)**1*3/-4";
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        System.out.println("Digite a entrada: ");
+        input = scanner.nextLine();
+        System.out.println(input);
+        Lexer lexer = new Lexer(new StringReader(input));
         lexer.yylex();
     }
 }
--- src/main/java/br/com/compiler/scanner/GeneratorScanner.java
+++ src/main/java/br/com/compiler/scanner/GeneratorScanner.java
@@ -8,8 +8,8 @@ public class GeneratorScanner {
     public static void main(String[] args) {
         String rootPath = Paths.get("").toAbsolutePath().toString();
         String subPath = "/src/main/java/br/com/compiler/scanner/";
-        String ﬁle = rootPath + subPath + "example.lex";
-        File sourceCode = new File(ﬁle);
+        String file = rootPath + subPath + "example.lex";
+        File sourceCode = new File(file);
         jflex.Main.generate(sourceCode);
     }
 }
