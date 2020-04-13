5092e607892d82c89b4d1466e00244a9f1f75175
jpbandeiralima <jpbandeiralima@gmail.com>
2020-04-13 03:20:12 -0300

    Alterações

--- src/main/java/br/com/compiler/scanner/CalculatorTest.java
+++ src/main/java/br/com/compiler/scanner/CalculatorTest.java
@@ -5,7 +5,7 @@ import java.io.StringReader;
 
 public class CalculatorTest {
     public static void main(String[] args) throws IOException {
-        String expression = "a2-2";
+        String expression = "-(8+12)**1*3/-4";
         Lexer lexer = new Lexer(new StringReader(expression));
         lexer.yylex();
     }
